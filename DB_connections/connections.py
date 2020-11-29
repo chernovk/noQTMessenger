c601de79276168f4e24ed35a33b265632d9677ce
PheeNeeck <Ustas184@gmail.com>
2020-11-29 20:05:58 +0300

    Развернул БД на Azure, исправил подключение

--- DB_connections/connections.py
+++ DB_connections/connections.py
@@ -1,18 +1,15 @@
 import pyodbc
 
-username = "Ravenom"
-password = "Ravenom01"
-database = "messenger"
 
+username = "Ravenom@mymessenger"
+password = "!ko393390"
+database = "messenger"
 
 def connection():
     con = pyodbc.connect('DRIVER={Devart ODBC Driver for MySQL};'
-                         f'User ID={username};'
-                         f'Password={password};'
-                         'Server=localhost;'
-                         f'Database={database};'
-                         'Port=3306;'
-                         'String Types=Unicode')
+                             f'User ID={username};'
+                             f'Password={password};'
+                             'Server=mymessenger.mysql.database.azure.com;'
+                             f'Database={database};'
+                             'Port=3306;')
     return con
-
-
