6e84fe91fa01902afc624235aaa6e81ea052778e
PheeNeeck <Ustas184@gmail.com>
2020-11-29 12:44:42 +0300

    Сделал модуль авторизации (получения токена) с API

---
+++ authorize/auth.py
@@ -0,0 +1,38 @@
+from DB_connections import connections
+import random
+
+
+class GetToken:
+    def __init__(self, login: str, password: str):
+        self.login = login
+        self.password = password
+
+    def get_token(self):
+        """
+        Метод по полученныму логину получает из БД данные пользователя, проверяет
+        наличие в базе логина и соответствие пароля логину и пытается записать в БД
+        новый токен для данного пользователя.
+        :return: token, при успешном внесении токена в базу
+                 False при невозможности внесения токена в базу по полученным параметрам
+        """
+        con = connections.connection()
+        with con:
+            cursor = con.cursor()
+            cursor.execute(f"SELECT `login`, `password`, `user_id` FROM `messenger`.`users` \
+                           WHERE `login` = '{self.login}';")
+            result = cursor.fetchall()
+            if result and result[0][1] == self.password:
+                while True:
+                    token = random.randint(1000000, 9999999)
+                    try:
+                        cursor.execute(f"INSERT INTO `messenger`.`tokens` (`user_id`, `token_number`) VALUES ('{result[0][2]}', '{token}');")
+                        con.commit()
+                        return token
+                    except:
+                        cursor.execute(f"UPDATE `messenger`.`tokens` SET `token_number` = '{token}' \
+                                WHERE (`user_id` = '{result[0][2]}');")
+                        con.commit()
+                        return token
+                    finally:
+                        pass
+            return False
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from reg.registration import AddUser
-
+from authorize.auth import GetToken
 
 app = Flask(__name__)
 
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def add_user():
     """
     Функция добавления нового пользователя. Принимает GET запрос в виде url с
-    обязательными параметрами login, password, name и пытается через класс AddUser
-    добавить пользователя в базу.
+    обязательными параметрами login, password, name от клиента и пытается через класс
+    AddUser добавить пользователя в базу.
     :return: Код 200 и соответствующее сообщение, если попытка удачна, иначе код 400
     """
     login = request.args.get("login")
@@ -21,6 +21,25 @@ def add_user():
         return make_response("Пользователь добавлен в базу!", 200)
     else:
         return make_response("Ошибка добавления!", 400)
+
+
+@app.route('/get_token/', methods=['GET'])
+def get_token():
+    """
+    Функция получает GET запрос с атрибутами login и password от пользовательского
+    сервиса, запрашивает по данным атрибутам токен для данного пользователя у класса
+    GetToken.
+    :return: token и код 200 при успешном получении токена
+             код 403 и сообщение об ошибке при отказе в получении токена
+    """
+    login = request.args.get("login")
+    password = request.args.get("password")
+    new_token = GetToken(login, password)
+    token = new_token.get_token()
+    if token:
+        return make_response(jsonify([token]), 200)
+    else:
+        return make_response("Ошибка авторизации!", 403)
 
 
 if __name__ == '__main__':
