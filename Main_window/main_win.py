36f396cc793149113a286e687b596e96d440a788
Ivan <ivan78.ru@yandex.ru>
2020-12-01 10:37:45 +0300

    Создал файлы интерфейса

--- Main_window/main_win.py
+++ Main_window/main_win.py
@@ -1,5 +1,19 @@
+import sys
+
+from PyQt5 import uic, QtWidgets
 
 
+class MainWindow(QtWidgets.QMainWindow, uic.loadUiType("main_window.ui")[0]):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
 
-def sdifjasdijf():
-    pass
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    a = MainWindow()
+    a.show()
+
+    sys.exit(app.exec_())
