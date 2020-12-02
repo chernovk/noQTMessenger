5d2727b445e1afb3745629c3ba121faa4a2c8e34
Ivan <ivan78.ru@yandex.ru>
2020-12-02 16:11:00 +0300

    То, каким я хочу видеть клиент

---
+++ Client_funcs.py
@@ -0,0 +1,44 @@
+import requests
+
+
+def register(ip: str, port: int, login: str, password: str):
+
+    # Посылаем запрос на сервер в каком-нибудь таком виде и получаем ответ
+    response = requests.request('POST',
+                                url=f'https://{ip}:{port}/API/reg?login={login}&pass={password}')
+
+    # Достаем из ответа ответ и возвращаем его
+    otvet = response.text
+    return otvet
+
+
+def authorise(ip: str, port: int, login: str, password: str) -> str:
+
+    # Посылаем запрос на сервер в каком-нибудь таком виде и получаем ответ
+    response = requests.request('POST', url=f'https://{ip}:{port}/API/auth?login={login}&pass={password}')
+
+    # Достаем из ответа токен и возвращаем его
+    token = response.text
+    return token
+
+
+def send_message(ip: str, port: int, token: str, message: str, adresat: str):
+
+    # Посылаем запрос на сервер в каком-нибудь таком виде и получаем ответ
+    response = requests.request('POST',
+                                url=f'https://{ip}:{port}/API/send?token={token}&message={message}&adresat={adresat}')
+
+    # Достаем из ответа ответ и возвращаем его
+    otvet = response.text
+    return otvet
+
+
+def get_history(ip: str, port: int, token: str, adresat: str):
+
+    # Посылаем запрос на сервер в каком-нибудь таком виде и получаем ответ
+    response = requests.request('POST',
+                                url=f'https://{ip}:{port}/API/get_history?token={token}&adresat={adresat}')
+
+    # Достаем из ответа ответ и возвращаем его
+    otvet = response.text
+    return otvet
