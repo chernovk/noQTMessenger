dbec6b7eeed8d8984057a64611c95f0bcc3641f6
KonstantinChernov <Chernov-k@yandex.ru>
2020-12-04 01:41:29 +0300

    fixed chat_service, begin message_handler

--- chat_service/chat_service.py
+++ chat_service/chat_service.py
@@ -1,9 +1,10 @@
+import json
+
 from flask import Flask, request
 from flask_restful import Api, Resource
-from DB_connections import connections
-
-
 import pika
+
+from DB_connections import connections
 
 app = Flask(__name__)
 api = Api(app)
@@ -31,11 +32,17 @@ class SendMessage(Resource):
                                "ON Users.user_id = Tokens.user_id "
                                f"WHERE token={token}"
                                )
-                sender_login = cursor.fetchall()[0][0]
+                try:
+                    sender_login = cursor.fetchall()[0][0]
+                except IndexError:
+                    return "403 Доступ запрещен", 403
                 if sender_login:
                     # Открываем соединение с очередью (урл прописать который будет на сервере)
-                    parameters = pika.URLParameters('localhost')
-                    connection = pika.BlockingConnection(parameters)
+                    credentials = pika.PlainCredentials('guest', 'guest')
+                    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
+                                                                                   5672,
+                                                                                   '/',
+                                                                                   credentials))
                     channel = connection.channel()
 
                     login = request.form['login']
@@ -43,17 +50,22 @@ class SendMessage(Resource):
                     # Объявляем очередь, если еще не создана
                     channel.queue_declare(queue=login)
 
-                    package = {
+                    package = json.dumps({
                         'sender': sender_login,
                         'date': request.form['date'],
                         'message': message
-                    }
+                    })
 
                     channel.basic_publish(exchange='', routing_key=login, body=package)
                     connection.close()
                     return "Сообщение отправлено", 200
                 else:
+                    # урл прописать который будет на сервере
                     return "403 Доступ запрещен", 302, {'location': 'https://localhost:5000/login'}
+
+
+
+
 
 
 api.add_resource(SendMessage, '/sendmessage')
---
+++ chat_service/message_handler.py
@@ -0,0 +1,91 @@
+import json
+import logging
+
+from flask import Flask, jsonify, request, make_response
+from flask_restful import Api, Resource
+import sqlite3
+
+import pika
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class ReceiveMessage(Resource):
+    def post(self):
+        """
+        Выдаем клиенту сообщение по его токену из очеереди сообщений
+
+        :return:
+        """
+        token = request.form['token']
+        conn = sqlite3.connect("tokens")
+        with conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT Users.login "
+                           "FROM Tokens "
+                           "INNER JOIN Users "
+                           "ON Users.user_id = Tokens.user_id "
+                           f"WHERE token={token}"
+                           )
+            try:
+                my_login = cursor.fetchall()[0][0]
+            except IndexError:
+                return "403 Доступ запрещен", 403
+            if my_login:
+
+                to_receive = {}
+
+                credentials = pika.PlainCredentials('guest', 'guest')
+                connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
+                                                                               5672,
+                                                                               '/',
+                                                                               credentials))
+                channel = connection.channel()
+
+                queue = channel.queue_declare(queue=my_login)
+
+                method, properties, body = channel.basic_get(my_login)
+
+                if method:
+                    while queue.method.message_count > 0:
+                        body = json.loads(body)
+
+                        login_sender = body['sender']
+                        message = body['message']
+                        datetime = body['date']
+
+                        if login_sender in to_receive:
+                            to_receive[login_sender].update({datetime: message})
+                        else:
+                            to_receive.update({login_sender: {datetime: message}})
+
+                        con = sqlite3.connect("tokens")
+                        with con:
+                            cur = con.cursor()
+                            cur.execute(f"SELECT user_id "
+                                        "FROM Users "
+                                        f"WHERE login=?", [login_sender])
+                            sender_id = cur.fetchall()[0][0]
+                            cur.execute("SELECT user_id "
+                                        "FROM Users "
+                                        f"WHERE login=?", [my_login])
+                            receiver_id = cur.fetchall()[0][0]
+                            cur.execute("INSERT INTO Messages(sender_id, receiver_id, date, message) "
+                                        "VALUES(?, ?, ?, ?)", [sender_id, receiver_id, datetime, message])
+                            con.commit()
+
+                    to_receive = json.dumps(to_receive)
+
+                    res = make_response(to_receive)
+                    return res
+                return make_response('No content', 204)
+
+            else:
+                return make_response("403 Доступ запрещен", 403)
+
+
+api.add_resource(ReceiveMessage, '/receivemessage')
+
+if __name__ == '__main__':
+    app.run(debug=True)
