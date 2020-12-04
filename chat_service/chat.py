6d308fd24cfbd166d19171b005d4e612287b35b9
KonstantinChernov <Chernov-k@yandex.ru>
2020-12-04 23:01:23 +0300

    chat that helps to send and to receive messages. Check DB

---
+++ chat_service/chat.py
@@ -0,0 +1,148 @@
+import json
+
+from flask import Flask, request, jsonify, make_response
+from flask_restful import Api, Resource
+import pika
+
+from DB_connections import connections
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class SendMessage(Resource):
+    def post(self):
+        """
+        Принимаем от клиента сообщение, его токен, логин адресата и дату/время отправки.
+        Токен сверяем с БД. Если корректный:
+        Подключаемся к очереди RabbitMQ, кладем в очередь с названием логина адресата сообщение
+        и дату/время его отправки в JSON формате.
+
+        :return:
+        """
+        message = request.form['message']
+        if message:
+            token = request.form['token']
+            con = connections.connection()
+            with con:
+                cursor = con.cursor()
+                cursor.execute("SELECT Users.login "
+                               "FROM Tokens "
+                               "INNER JOIN Users "
+                               "ON Users.user_id = Tokens.user_id "
+                               f"WHERE token={token}"
+                               )
+                try:
+                    sender_login = cursor.fetchall()[0][0]
+                except IndexError:
+                    return "403 Доступ запрещен", 403
+                if sender_login:
+                    # Открываем соединение с очередью (урл прописать который будет на сервере)
+                    credentials = pika.PlainCredentials('guest', 'guest')
+                    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
+                                                                                   5672,
+                                                                                   '/',
+                                                                                   credentials))
+                    channel = connection.channel()
+
+                    login = request.form['login']
+
+                    # Объявляем очередь, если еще не создана
+                    channel.queue_declare(queue=login)
+
+                    package = json.dumps({
+                        'sender': sender_login,
+                        'date': request.form['date'],
+                        'message': message
+                    })
+
+                    channel.basic_publish(exchange='', routing_key=login, body=package)
+                    connection.close()
+                    return "Сообщение отправлено", 200
+                else:
+                    # урл прописать который будет на сервере
+                    return "403 Доступ запрещен", 302, {'location': 'https://localhost:5000/login'}
+
+
+class ReceiveMessage(Resource):
+    def post(self):
+        """
+        Выдаем клиенту сообщение по его токену из очеереди сообщений
+
+        :return:
+        """
+        token = request.form['token']
+        con = connections.connection()
+        with con:
+            cursor = con.cursor()
+            cursor.execute("SELECT Users.login "
+                           "FROM Tokens "
+                           "INNER JOIN Users "
+                           "ON Users.user_id = Tokens.user_id "
+                           f"WHERE token={token}"
+                           )
+            try:
+                my_login = cursor.fetchall()[0][0]
+            except IndexError:
+                return "403 Доступ запрещен", 403
+            if my_login:
+
+                to_receive = {}
+
+                credentials = pika.PlainCredentials('guest', 'guest')
+                connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
+                                                                               5672,
+                                                                               '/',
+                                                                               credentials))
+                channel = connection.channel()
+
+                queue = channel.queue_declare(queue=my_login)
+                count = queue.method.message_count
+
+                while count > 0:
+                    method_frame, properties, body = channel.basic_get(my_login)
+                    data = json.loads(body)
+
+                    login_sender = data['sender']
+                    message = data['message']
+                    datetime = data['date']
+
+                    if login_sender in to_receive:
+                        to_receive[login_sender].update({datetime: message})
+                    else:
+                        to_receive.update({login_sender: {datetime: message}})
+
+                    con = connections.connection()
+                    with con:
+                        cur = con.cursor()
+                        cur.execute(f"SELECT user_id "
+                                    "FROM Users "
+                                    f"WHERE login=?", [login_sender])
+                        sender_id = cur.fetchall()[0][0]
+                        cur.execute("SELECT user_id "
+                                    "FROM Users "
+                                    f"WHERE login=?", [my_login])
+                        receiver_id = cur.fetchall()[0][0]
+                        cur.execute("INSERT INTO Messages(sender_id, receiver_id, date, message) "
+                                    "VALUES(?, ?, ?, ?)", [sender_id, receiver_id, datetime, message])
+                        con.commit()
+                    channel.basic_ack(method_frame.delivery_tag)
+                    count -= 1
+                channel.close()
+                connection.close()
+                if len(to_receive):
+                    return jsonify(to_receive)
+                else:
+                    make_response("204 No Content", 204)
+            else:
+                return make_response("403 Доступ запрещен", 403)
+
+
+api.add_resource(SendMessage, '/sendmessage')
+
+api.add_resource(ReceiveMessage, '/receivemessage')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
