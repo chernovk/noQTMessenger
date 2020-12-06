990685cb7d504f78c048a6840ff6b0c1c90f0e06
Pheeneek <64259071+Pheeneek@users.noreply.github.com>
2020-12-06 20:47:37 +0300

    Update chat.py
    Перелопатил маленько код. Вынес в отдельный класс проверку пост запроса на валидность. Первая функция посылки сообщения работает, проверил, вторую проработал запросы, но не успел проверить. По идее, все должно работать, тут полагаюсь на правильность кода работы с Кроликом. Немного изменил смысл: в первой функции получается сообщение, записывается в базу с полем received = False. Во второй функции, при выдаче сообщения из очереди получателю, значение поля received меняется на True.
    Надо доделать: 
    1) прописать нормальные Exception  в Try-Except, пока заглушки поставил просто.
    2) Хорошо бы сделать логирование.
    3) Проверить правильность отработки второй функции.
    4) По возможности, разбить код на несколько более коротких функций, читать сложно и понимать что к чему.

--- chat_service/chat.py
+++ chat_service/chat.py
@@ -1,87 +1,122 @@
 import json
-
 from flask import Flask, request, jsonify, make_response
-from flask_restful import Api, Resource
 import pika
+import config
 
 from DB_connections import connections
 
-app = Flask(__name__)
-api = Api(app)
 
+class Verification:
+    def __init__(self, message, token, receiver):
+        self.message = message
+        self.token = token
+        self.receiver = receiver
 
-class SendMessage(Resource):
-    def post(self):
-        """
-        Принимаем от клиента сообщение, его токен, логин адресата и дату/время отправки.
-        Токен сверяем с БД. Если корректный:
-        Подключаемся к очереди RabbitMQ, кладем в очередь с названием логина адресата сообщение
-        и дату/время его отправки в JSON формате.
-
-        :return:
-        """
-        message = request.form['message']
-        if message:
-            token = request.form['token']
-            con = connections.connection()
-            with con:
-                cursor = con.cursor()
-                cursor.execute("SELECT Users.login "
-                               "FROM Tokens "
-                               "INNER JOIN Users "
-                               "ON Users.user_id = Tokens.user_id "
-                               f"WHERE token={token}"
-                               )
-                try:
-                    sender_login = cursor.fetchall()[0][0]
-                except IndexError:
-                    return "403 Доступ запрещен", 403
-                if sender_login:
-                    # Открываем соединение с очередью (урл прописать который будет на сервере)
-                    credentials = pika.PlainCredentials('guest', 'guest')
-                    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
-                                                                                   5672,
-                                                                                   '/',
-                                                                                   credentials))
-                    channel = connection.channel()
-
-                    login = request.form['login']
-
-                    # Объявляем очередь, если еще не создана
-                    channel.queue_declare(queue=login)
-
-                    package = json.dumps({
-                        'sender': sender_login,
-                        'date': request.form['date'],
-                        'message': message
-                    })
-
-                    channel.basic_publish(exchange='', routing_key=login, body=package)
-                    connection.close()
-                    return "Сообщение отправлено", 200
-                else:
-                    # урл прописать который будет на сервере
-                    # TODO
-                    return "403 Доступ запрещен", 302, {'location': 'https://localhost:5000/login'}
-
-
-class ReceiveMessage(Resource):
-    def post(self):
-        """
-        Выдаем клиенту сообщение по его токену из очеереди сообщений
-        По моей идее, клиент регулярно посылает POST запрос на сервер, запрашивая новые сообщения для него.
-        в этом запросе он передает свой токен (поэтому пост запрос, т.к. токен не должен быть в урле)
-
-        :return:
-        """
-        token = request.form['token']
+    def __verify_data__(self):
         con = connections.connection()
-        with con:
+        if not self.message:
+            return False
+
+        try:
             cursor = con.cursor()
-            cursor.execute("SELECT Users.login "
-                           "FROM Tokens "
-                           "INNER JOIN Users "
-                           "ON Users.user_id = Tokens.user_id "
+            cursor.execute(f"SELECT login, token_number "
+                           f"FROM users "
+                           f"WHERE token_number = '{self.token}';")
+            result = cursor.fetchall()
+            if not str(result[0][1]) == self.token:
+                return False
+        except Exception:
+            return False
+
+        try:
+            cursor = con.cursor()
+            cursor.execute(f"SELECT login "
+                           f"FROM users "
+                           f"WHERE login = '{self.receiver}';")
+            result = cursor.fetchall()
+            if not result[0][0]:
+                return False
+        except:
+            return False
+        return True
+
+
+class StartChat:
+    def __init__(self, adress, port):
+        self.adress = adress
+        self.port = port
+        app = Flask(__name__)
+
+        @app.route("/send_message/", methods=["POST"])
+        def receive_message():
+            """
+            Принимаем от клиента сообщение, его токен, логин адресата и дату/время отправки.
+            Токен сверяем с БД. Если корректный:
+            Подключаемся к очереди RabbitMQ, кладем в очередь с названием логина адресата сообщение
+            и дату/время его отправки в JSON формате.
+            :return:
+            """
+            message = request.form['message']
+            token = request.form["token"]
+            receiver = request.form["receiver"]
+            time = request.form["time"]
+
+            verify_message = Verification(message, token, receiver)
+            if verify_message.__verify_data__():
+
+                credentials = pika.PlainCredentials('guest', 'guest')
+                connection = pika.BlockingConnection(pika.ConnectionParameters(config.rabbit_adress,
+                                                                               config.rabbit_port,
+                                                                               '/',
+                                                                               credentials))
+                channel = connection.channel()
+                con = connections.connection()
+                cursor = con.cursor()
+                cursor.execute(f"SELECT login, user_id "
+                               f"FROM messenger.users "
+                               f"WHERE token_number = '{token}';")
+                result = cursor.fetchall()
+                login = result[0][0]
+                sender_id = result[0][1]
+                cursor.execute(f"SELECT user_id "
+                               f"FROM messenger.users "
+                               f"WHERE login = '{receiver}';")
+                receiver_id = cursor.fetchall()[0][0]
+                try:
+                    cursor.execute(
+                        f"INSERT INTO messenger.messages (sender_id, receiver_id, message, send_time, received)"
+                        f" VALUES ('{sender_id}', '{receiver_id}', '{message}', '{time}', '{False}');"
+                    )
+                    con.commit()
+                    print("Сообщение успешно добавлено в базу!")
+                except Exception:
+                    print("Не удалось записать сообщение в базу!")
+
+                channel.queue_declare(queue=login)
+                package = json.dumps({
+                    'sender': login,
+                    'date': time,
+                    'message': message
+                })
+                channel.basic_publish(exchange='', routing_key=login, body=package)
+                connection.close()
+                return make_response(jsonify([str("Successfully send!"), 200]))
+            else:
+                return make_response(jsonify([str("Wrong request!"), 400]))
+
+        @app.route("/give_message/", methods=["GET"])
+        def give_message():
+            """
+            Выдаем клиенту сообщение по его токену из очереди сообщений
+            По моей идее, клиент регулярно посылает POST запрос на сервер, запрашивая новые сообщения для него.
+            в этом запросе он передает свой токен (поэтому пост запрос, т.к. токен не должен быть в урле)
+            :return:
+            """
+            token = request.args.get('token')
+            con = connections.connection()
+            cursor = con.cursor()
+            cursor.execute("SELECT login "
+                           "FROM users "
                            f"WHERE token={token}"
                            )
             # По токену клиент устанавливает свой логин, для запроса сообщений из своей очереди, если токен не валидный,
@@ -89,16 +124,14 @@ class ReceiveMessage(Resource):
             try:
                 my_login = cursor.fetchall()[0][0]
             except IndexError:
-                # TODO
-                return "403 Доступ запрещен", 403
+                return make_response(["403 Доступ запрещен", 403])
             if my_login:
-
                 # создаем словарик, в который будем помещать имеющиеся в очереди сообщения
                 to_receive = {}
 
                 credentials = pika.PlainCredentials('guest', 'guest')
-                connection = pika.BlockingConnection(pika.ConnectionParameters('localhost',
-                                                                               5672,
+                connection = pika.BlockingConnection(pika.ConnectionParameters(config.rabbit_adress,
+                                                                               config.rabbit_port,
                                                                                '/',
                                                                                credentials))
                 channel = connection.channel()
@@ -125,19 +158,21 @@ class ReceiveMessage(Resource):
                         to_receive.update({login_sender: {datetime: message}})
 
                     con = connections.connection()
-                    with con:
-                        cur = con.cursor()
-                        cur.execute(f"SELECT user_id "
-                                    "FROM Users "
-                                    f"WHERE login=?", [login_sender])
-                        sender_id = cur.fetchall()[0][0]
-                        cur.execute("SELECT user_id "
-                                    "FROM Users "
-                                    f"WHERE login=?", [my_login])
-                        receiver_id = cur.fetchall()[0][0]
-                        cur.execute("INSERT INTO Messages(sender_id, receiver_id, date, message) "
-                                    "VALUES(?, ?, ?, ?)", [sender_id, receiver_id, datetime, message])
-                        con.commit()
+                    cur = con.cursor()
+                    cur.execute(f"SELECT user_id "
+                                "FROM Users "
+                                f"WHERE login='{login_sender}';")
+                    sender_id = cur.fetchall()[0][0]
+                    cur.execute("SELECT user_id "
+                                "FROM Users "
+                                f"WHERE login='{my_login}';")
+                    receiver_id = cur.fetchall()[0][0]
+                    cur.execute(f"UPDATE messages SET received ='{True}' "
+                                f"WHERE message = '{message}', "
+                                f"sender_id = '{sender_id}', "
+                                f"receiver_id ='{receiver_id}' "
+                                f"send_time = '{datetime}';")
+                    con.commit()
                     channel.basic_ack(method_frame.delivery_tag)
                     count -= 1
                 channel.close()
@@ -146,17 +181,12 @@ class ReceiveMessage(Resource):
                 if len(to_receive):
                     return jsonify(to_receive)
                 else:
-                    make_response("204 No Content", 204)
+                    make_response(jsonify(["204 No Content", 204]))
             else:
-                # TODO
-                return make_response("403 Доступ запрещен", 403)
+                return make_response(jsonify(["403 Доступ запрещен", 403]))
+
+        app.run(f"{self.adress}", port=self.port)
 
 
-api.add_resource(SendMessage, '/sendmessage')
-
-api.add_resource(ReceiveMessage, '/receivemessage')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
+StartChat(config.chat_adress, config.chat_port)
 
